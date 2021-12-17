8afcc4d270846b35d1f9b181e8f83c22378bb032
Saleh Ahmed Panna <panna.ahmed09@gmail.com>
2021-12-17 14:54:55 +0600

    Fixed auth  flow

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+    renderContent(){
+        switch(this.props.auth){
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with google</a></li>;
+            default:
+                return <li><a href="/api/logout">logout</a></li>;
+        }
+    }
+
     render(){
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/'} 
+                        className="left brand-logo"
+                    >
                         Tasty
-                    </a>
+                    </Link>
                     <ul className="right">
                         <li>
-                            Login with google
+                            {this.renderContent()}
                         </li>
                     </ul>
                 </div>
@@ -19,4 +35,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }){
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Landing try</h1>
+            Lets see
+        </div>
+    );
+}
+
+export default Landing;
